2865a7e42994538e3e3c20fc5661a03f27973d22
laylatame <laylatame@hotmail.com>
2020-02-03 18:33:10 -0600

    Open email window

---
+++ public/js/api_calls/contactUs.js
@@ -0,0 +1,54 @@
+function sendMail(mailto, message) {
+  var link =
+    "mailto:xuma@contactus.com" +
+    "?cc=" +
+    "&subject=" +
+    escape("XUMA - Gracias por contactarnos!") +
+    "&body=" +
+    message;
+
+  window.location.href = link;
+}
+
+$("#sendMessageButton").on("click", function() {
+  let name = $("#nameForm").val();
+  let email = $("#emailForm").val();
+  let phone = $("#phoneForm").val();
+  let message = $("#messageForm").val();
+
+  if (!name || !email || !phone || !message) {
+    alert("Porfavor llene todos los campos.");
+  } else {
+    $("#nameForm").val("");
+    $("#emailForm").val("");
+    $("#phoneForm").val("");
+    $("#messageForm").val("");
+    sendMail(email, message);
+    // $.ajax({
+    //   type: "POST",
+    //   url: "https://mandrillapp.com/api/1.0/messages/send.json",
+    //   data: {
+    //     key: "9544e3e101700147ebae5b962a787172-us4",
+    //     message: {
+    //       from_email: "a01192934@itesm.mx",
+    //       to: [
+    //         {
+    //           email: email,
+    //           name: name,
+    //           type: "to"
+    //         }
+    //       ],
+    //       autotext: "true",
+    //       subject: "Xuma - Gracias por contactarnos!",
+    //       html: message
+    //     }
+    //   }
+    // }).done(function(response) {
+    //   console.log(response);
+    // });
+
+    let button = $("#sendMessageButton");
+    button[0].innerText = "✔ ENVIADO";
+    alert("Muchas gracias! Nos podremos en contacto pronto.");
+  }
+});
